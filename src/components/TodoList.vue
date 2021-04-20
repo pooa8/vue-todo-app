8e99b1706d40e122f90c3f7657df695bc3fd5f66
leear <pooa8@naver.com>
2021-04-20 23:26:15 +0900

    Refactor : TodoList delete function refactoring - 할 일 삭제 기능 리팩토링 - TodoList.vue, App.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -17,9 +17,7 @@ export default {
   props: ['propsdata'],
   methods: {
     removeTodo: function(todoItem, index) {
-      console.log(todoItem, index);
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index, 1);
+      this.$emit('removeItem', todoItem, index);
     },
     toggleComplete: function(todoItem, index) {
       todoItem.completed = !todoItem.completed;
